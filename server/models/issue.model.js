1973556e4772215ef4e72ebecaf0049928391ed4
Sartori <xandy.sartori@gmail.com>
2025-12-30 17:00:19 -0300

    adição de issues via sockets

--- server/models/issue.model.js
+++ server/models/issue.model.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const issueSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
-  node_id: { type: String, required: false },
-  project_url: { type: String, required: false },
-  content_type: { type: String, required: false },
-  created_at: { type: String, required: false },
-  updated_at: { type: String, required: false },
-  archived_at: { type: String, required: false },
-  item_url: { type: String, required: false },
-  votes: { type: Array },
-  show_votes: { type: Boolean, default: false },
+    id: { type: Number, required: true, unique: true },
+    node_id: { type: String, required: false },
+    project_url: { type: String, required: false },
+    content_type: { type: String, required: false },
+    created_at: { type: String, required: false },
+    updated_at: { type: String, required: false },
+    archived_at: { type: String, required: false },
+    item_url: { type: String, required: false },
+    votes: { type: Array, default: [] },
+    show_votes: { type: Boolean, default: false },
 });
 
 const Issue = mongoose.model("Issue", issueSchema);
